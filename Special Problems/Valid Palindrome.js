cfebe9f1f76176c8e56f765c4e774c44dda4ef21
nelasunitha <nelasunitha@gmail.com>
2022-12-13 19:11:45 -0800

    alternate solution to valid palindrome

--- Special Problems/Valid Palindrome.js
+++ Special Problems/Valid Palindrome.js
@@ -32,3 +32,22 @@ function isPalindrome(s) {
 console.log(isPalindrome("A man, a plan, a canal: Panama"));
 console.log(isPalindrome("race a car"));
 console.log(isPalindrome(" "));
+
+/* without using regex, using ascII values*/
+var isPalindrome = function(s) {
+  const strArr = [];
+  for(let i= 0; i < s.length; i++) {
+
+     if(s.charCodeAt(i)>= 65 && s.charCodeAt(i)<91 ||( s.charCodeAt(i)>= 97 && s.charCodeAt(i)<123 ) || (s.charCodeAt(i)>= 48 && s.charCodeAt(i)<58) ) {
+      strArr.push(s[i].toLowerCase())
+      }
+  }
+   const str = strArr.join('');
+ //   if(str.length === 1) return true
+   console.log('s',str, str.length )
+  for(let i =0, j=str.length-1; i<j; i++, j--) {
+      if(str[i]!== str[j]) return false
+  }
+  return true;
+
+ };
